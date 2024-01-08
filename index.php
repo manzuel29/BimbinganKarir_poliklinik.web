9ad028b843613116a1d8214931a9a249b8ae8fae
M NASHWA ZULFAN SYAH <113042808+manzuel29@users.noreply.github.com>
2024-01-08 09:27:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 session_start();
 
 if (isset($_SESSION['username'])) {
-    if ($_SESSION['role'] === 'dokter' && (!isset($_GET['page']) || !in_array($_GET['page'], ['index','dokter','pasien', 'periksa','detail_periksa','obat','riwayat','']))) {
+    if ($_SESSION['role'] === 'dokter' && (!isset($_GET['page']) || !in_array($_GET['page'], ['index','dokter','pasien', 'periksa','detail_periksa','obat','riwayat','jadwal_periksa','daftar_poli']))) {
         header('Location: index.php?page=dokter');
         exit;
-    } elseif ($_SESSION['role'] === 'admin' && (!isset($_GET['page']) || !in_array($_GET['page'], ['dokter','pasien','poli','obat']))) {
+    } elseif ($_SESSION['role'] === 'admin' && (!isset($_GET['page']) || !in_array($_GET['page'], ['dokter','pasien','poli','obat','jadwal_periksa']))) {
         header('Location: index.php?page=pasien');
         exit;
-    } elseif ($_SESSION['role'] === 'pasien' && (!isset($_GET['page']) || !in_array($_GET['page'], ['lihat_jadwal_dokter','daftar_pasien','daftar_poli_pasien','riwayat']))) {
-        header('Location: index.php?page=daftar_pasien');
+    } elseif ($_SESSION['role'] === 'pasien' && (!isset($_GET['page']) || !in_array($_GET['page'], ['lihat_jadwal_dokter','daftar_poli','riwayat','pasien']))) {
+        header('Location: index.php?page=pasien');
         exit;
     }
     if(isset($_GET['page'])&& $_GET['page']==='periksa'&& empty($_SESSION['role'])){
@@ -22,87 +22,80 @@ include_once("koneksi.php");
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
+    <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sistem Informasi Poliklinik</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
+    </head>
+    <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
+    <div class="container-fluid">
             <a class="navbar-brand" href="#">Sistem Informasi Poliklinik</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
-                    </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="index.php?page=daftar_pasien">Daftar Pasien</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="index.php?page=lihat_jadwal_dokter">Lihat Jadwal Dokter</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="index.php?page=daftar_poli_pasien">Daftar Poli Pasien</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="index.php?page=riwayat">Riwayat</a>
-                            </li>
-                            
-                    </li>
-                </ul>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Data Master</a>
-                        <ul class="dropdown-menu">
-                        <a>----Kelola Dokter----</a>
-                        <li>
-                                <a class="dropdown-item" href="index.php?page=dokter">Dokter</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="index.php?page=pasien">Pasien</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="index.php?page=daftar_poli">Daftar Poli</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="index.php?page=periksa">Periksa</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="index.php?page=obat">Obat</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="index.php?page=detail_periksa">Detail Periksa</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="index.php?page=riwayat">Riwayat</a>
-                            </li>
-                            <a>----Kelola Admin----</a>
-                            <li>
-                                <a class="dropdown-item" href="index.php?page=dokter">Dokter</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="index.php?page=pasien">Pasien</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="index.php?page=poli">Poli</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="index.php?page=obat">Obat</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="index.php?page=jadwal_periksa">Jadwal Periksa</a>
-                            </li>
-                        </ul>
-                    </li>>
-                </ul>
-            </div>
-           
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="nav-link text-white" href="index.php">Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav me-auto">
+                <?php if (isset($_SESSION['username'])) { ?>
+                    <?php if ($_SESSION['role'] === 'pasien') { ?>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=daftar_poli_pasien">Daftar Poli Pasien</a>
+                        </li>
+                    <?php } elseif ($_SESSION['role'] === 'dokter') { ?>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=dokter">Dokter</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=pasien">Pasien</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=periksa">Periksa</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=detail_periksa">Detail Periksa</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=obat">Obat</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=riwayat">Riwayat</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=daftar_poli">Daftar Poli</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=jadwal_periksa">Jadwal Periksa</a>
+                        </li>
+                    <?php } elseif ($_SESSION['role'] === 'admin') { ?>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=dokter">Dokter</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=pasien">Pasien</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=poli">Poli</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=obat">Obat</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php?page=jadwal_periksa">Jadwal Periksa</a>
+                        </li>
+                    <?php } ?>
+                <?php } ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+
                 <?php
                 if (isset($_SESSION['username'])) {
                     // Jika pengguna sudah login, tampilkan tombol "Logout"
@@ -144,9 +137,9 @@ include_once("koneksi.php");
                     echo "</h2><hr>Silakan Login untuk menggunakan sistem. Jika belum memiliki akun silakan Register terlebih dahulu.";
                 }
             }
-     ?>
+    ?>
     
 </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
